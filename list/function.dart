f169c76243b58b9990df728ff8427e175546bd45
estiesten <estidwi473@gmail.com>
2023-03-11 15:27:59 +0700

    input coding pertemuan 1

---
+++ list/function.dart
@@ -0,0 +1,16 @@
+void main() {
+  halo();
+  print(penjumlahan(5, 5));
+}
+
+void halo(){
+  print('Selamat sore gess!');
+}
+
+double penjumlahan(double x, y) {
+  double z = x+y;
+
+  return z;
+}
+
+// fungsi halo harus dipanggil di main agar bisa dirunning seperti kolom no 2
---
+++ list/list.dart
@@ -0,0 +1,22 @@
+void main(){
+  //Deklarasi List
+  var listString = <string>["Esti", "Deka", "Fifi", "Elsa"];
+  List<int> angka = [1,2,3,4];
+
+  // angka.add(1);
+  // angka.add(2);
+  angka.remove(3);
+
+  // print(listString.length);
+
+  // for (int a=0; a<=3; a++) {
+    // print(listening[a]);
+  // }
+
+  angka.forEach((a){
+    print(a);
+  });
+
+  // print(angka[2]);
+  // print(listring[2]);
+}
